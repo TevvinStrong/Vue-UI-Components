f0b5e8df7afee6b51d45733d48ad889dab12b59d
Tevvin <Tevvin7395@icloud.com>
2020-08-22 00:33:46 -0400

    CardContainer rendering

---
+++ social-card/src/components/CardContainer.vue
@@ -0,0 +1,15 @@
+<!--Markup/HTML-->
+<template>
+  <div class="container">
+    <h1>Hello, World!</h1>
+  </div>
+</template>
+
+<!--Javascript-->
+<script>
+export default {};
+</script>
+
+<!--CSS-->
+<style scoped>
+</style>
